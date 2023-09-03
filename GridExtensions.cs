ea7822704d5d3837449687508e1c7c67f23d08bc
Mio's Fluffy Ears <miosfluffyears@proton.me>
2023-09-04 01:57:29 +1000

    First working version

---
+++ GridExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+
+namespace JSONViewer.NET
+{
+    public static class GridExtensions
+    {
+        public static Border CreateBorder(this Grid grid, int columnIndex, int rowIndex)
+        {
+            var border = new Border();
+            border.BorderBrush = new SolidColorBrush(Colors.Black);
+            border.BorderThickness = new Microsoft.UI.Xaml.Thickness(1);
+            border.SetValue(Grid.ColumnProperty, columnIndex);
+            border.SetValue(Grid.RowProperty, rowIndex);
+            border.Padding = new Microsoft.UI.Xaml.Thickness(5);
+
+            grid.Children.Add(border);
+            return border;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -20,6 +21,7 @@ namespace JSONViewer.NET
 {
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
+    /// no Frame?.jpg
     /// </summary>
     public sealed partial class MainWindow : Window
     {
@@ -28,9 +30,83 @@ namespace JSONViewer.NET
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void start_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            using (JsonDocument document = JsonDocument.Parse(input.Text))
+            {
+                var root = document.RootElement;
+                render(output, root);
+            }
+        }
+
+        private void render(Panel destination, JsonElement node)
+        {
+            switch (node.ValueKind)
+            {
+                case JsonValueKind.Undefined:
+                    break;
+                case JsonValueKind.Object:
+                    var objectGrid = new Grid();
+                    //{
+                    //    Style = (Style)Application.Current.Resources["ObjectGrid"]
+                    //};
+                    objectGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                    objectGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                    var i = 0;
+                    foreach(var pair in node.EnumerateObject())
+                    {
+                        objectGrid.RowDefinitions.Add(new RowDefinition());
+                        objectGrid.RowDefinitions.Add(new RowDefinition());
+
+                        var nameBorder = objectGrid.CreateBorder(0, i);
+                        var valueBorder = objectGrid.CreateBorder(1, i);
+
+                        var nameText = new TextBlock
+                        {
+                            Text = pair.Name
+                        };
+                        nameBorder.Child = nameText;
+
+                        var value = new StackPanel();
+                        value.Orientation = Orientation.Vertical;
+                        value.HorizontalAlignment = HorizontalAlignment.Stretch;
+                        value.VerticalAlignment = VerticalAlignment.Top;
+
+                        valueBorder.Child = value;
+
+                        render(value, pair.Value);
+
+                        i++;
+                    }
+
+                    destination.Children.Add(objectGrid);
+                    break;
+                case JsonValueKind.Array:
+                    break;
+                case JsonValueKind.String:
+                    var stringOutput = new TextBlock
+                    {
+                        Text = node.GetString()
+                    };
+                    destination.Children.Add(stringOutput);
+
+                    break;
+                case JsonValueKind.Number:
+                    var numberOutput = new TextBlock
+                    {
+                        Text = node.ToString()
+                    };
+                    destination.Children.Add(numberOutput);
+                    break;
+                case JsonValueKind.True:
+                    break;
+                case JsonValueKind.False:
+                    break;
+                case JsonValueKind.Null:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
